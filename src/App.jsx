2951b1e13cbb533f75d78a1151d01043aabf0858
Racheal152 <kacheracheal152@gmail.com>
2025-06-19 12:50:19 +0300

    Complete and Responsive Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className='text-red-500 flex justify-center'>My App</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
